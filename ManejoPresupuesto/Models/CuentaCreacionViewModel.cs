c19207bd5a6a2a29105dd4f1a1f4a5d28be176d2
jorgelgpz <jorge.lgpz@gmail.com>
2023-12-31 13:40:31 -0600

    Formulario de Cuentas

---
+++ ManejoPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models
+{
+    public class CuentaCreacionViewModel : Cuenta
+    {
+        //SelectListItem es una clase que me permite crear selects facilmente 
+        public IEnumerable<SelectListItem> TiposCuentas { get; set; }
+    }
+}
