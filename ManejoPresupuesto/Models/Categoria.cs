801d21cad2dd4aa95b997f62ff013546a3106112
jorgelgpz <jorge.lgpz@gmail.com>
2024-01-02 11:10:51 -0600

    Formulario Categorías - Creando un Select de un Enum

--- ManejoPresupuesto/Models/Categoria.cs
+++ ManejoPresupuesto/Models/Categoria.cs
@@ -7,7 +7,8 @@ namespace ManejoPresupuesto.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 50, ErrorMessage = "No puede ser mayor a {1} caracteres")]
-        public int Nombre { get; set; }
+        public string Nombre { get; set; }
+        [Display(Name ="Tipo Operación")]
         public TipoOperacion TipoOperacionId { get; set; }
         public int UsuarioId { get; set; }
     }
