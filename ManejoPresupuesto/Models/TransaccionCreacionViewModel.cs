bf25f189895045cff520d95e4f802f29ecc93a39
jorgelgpz <jorge.lgpz@gmail.com>
2024-01-03 15:29:37 -0600

    Actualizando Transacciones - Parte 1

--- ManejoPresupuesto/Models/TransaccionCreacionViewModel.cs
+++ ManejoPresupuesto/Models/TransaccionCreacionViewModel.cs
@@ -7,7 +7,5 @@ namespace ManejoPresupuesto.Models
     {
         public IEnumerable<SelectListItem> Categorias { get; set; }
         public IEnumerable<SelectListItem> Cuentas { get; set; }
-        [Display(Name = "Tipo Operación")]
-        public TipoOperacion TipoOperacionId { get; set; } = TipoOperacion.Ingreso;
     }
 }
--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -6,7 +6,9 @@ namespace ManejoPresupuesto.Servicios
 {
     public interface IRepositorioTransacciones
     {
+        Task Actuliazar(Transaccion transaccion, decimal montoAnterior, int cuentaAnterio);
         Task Crear(Transaccion transaccion);
+        Task<Transaccion> ObtenerPorId(int id, int usuarioId);
     }
     public class RepositorioTransacciones : IRepositorioTransacciones
     {
@@ -34,6 +36,36 @@ namespace ManejoPresupuesto.Servicios
                 );
 
             transaccion.Id = id;
+        }
+
+        public async Task Actuliazar(Transaccion transaccion, decimal montoAnterior,
+            int cuentaAnterio)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync("Transacciones_Actualizar",
+                new
+                {
+                    transaccion.Id,
+                    transaccion.FechaTransaccion,
+                    transaccion.Monto,
+                    transaccion.CategoriaId,
+                    transaccion.CuentaId,
+                    transaccion.Nota,
+                    montoAnterior,
+                    cuentaAnterio
+                }, commandType: System.Data.CommandType.StoredProcedure);
+        }
+
+        public async Task<Transaccion> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<Transaccion>(
+                @"SELECT Transacciones.*, cat.TipoOperacionId
+                FROM Transacciones
+                INNER JOIN Categorias cat
+                ON cat.Id = Transacciones.CategoriaId
+                WHERE Transacciones.Id = @Id AND Transacciones.UsuarioId = @UsuarioId;",
+                new { id, usuarioId });
         }
     }
 }
